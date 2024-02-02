6d20b718ed9d710dbbbb273b4574078fb5570809
KazKinjo <kinjo.kazuaki@gmail.com>
2024-02-02 14:52:42 +0800

    Updated WelcomeScreen components including TextInput, KeyboardAvoidingView and Platform

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import MenuItems from './components/MenuItems';
+import WelcomeScreen from './components/WelcomeScreen';
+import LoginScreen from './components/LoginScreen';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <Header />
-        <MenuItems />
+        <LoginScreen />
       </View>
       <View>
+        <Footer />
       </View>
     </>
   );
--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,17 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, TextInput } from "react-native";
 
 const WelcomeScreen = () => {
+  const [firstname, onChangeFirstname] = useState('');
+  const [lastname, onChangeLastname] = useState('');
+  const [message, onChangeMessage] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>
-        Welcome to Little Lemon
-      </Text>
-      <Text style={styles.text}>
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
-      </Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <ScrollView keyboardDismissMode="on-drag">
+        <Text style={styles.headerText}>
+          Welcome to Little Lemon
+        </Text>
+        <Text style={styles.text}>
+          Little Lemon is a charming neighborhood bistro that serves simple food
+          and classic cocktails in a lively but casual environment. We would love
+          to hear more about your experience with us!
+        </Text>
+        <TextInput
+          style={styles.input}
+          value={firstname}
+          placeholder={"Firstname"}
+          onChangeText={onChangeFirstname}
+        />
+        <TextInput
+          style={styles.input}
+          value={lastname}
+          placeholder={"Lastname"}
+          onChangeText={onChangeLastname}
+        />
+        <TextInput
+          style={styles.messageInput}
+          value={message}
+          placeholder={"Please leave feedback"}
+          onChangeText={onChangeMessage}
+          multiline={true}
+          maxLength={250}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 };
 
@@ -19,7 +46,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  heading: {
+  headerText: {
     fontSize: 30,
     color: '#EDEFEE',
     textAlign: 'center',
@@ -31,6 +58,24 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 20,
     marginVertical: 8,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: "#EDEFEE",
+    backgroundColor: "#EDEFEE",
+    padding: 10,
+    fontSize: 16,
+  },
+  messageInput: {
+    height: 100,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: "#EDEFEE",
+    backgroundColor: "#EDEFEE",
+    padding: 10,
+    fontSize: 16,
   }
 });
 
